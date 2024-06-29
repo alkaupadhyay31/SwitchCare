81bb5c48ec2346fc734fe1b553af755a53726716
alkaupadhyay31 <alkaupadhyay31@gmail.com>
2024-06-29 13:57:34 +0530

    Switch health care application

---
+++ src/main/java/SwitchCare/SwitchCapabilities.java
@@ -0,0 +1,30 @@
+package SwitchCare;
+import java.net.MalformedURLException;
+
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+
+public class SwitchCapabilities {
+	
+public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException{
+		
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "My_emulator");
+		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.dcs.provider");
+		dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.dcs.provider.MainActivity");
+		dc.setCapability(MobileCapabilityType.NO_RESET, true);
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"), dc);
+		return driver;
+		
+		
+	}
+
+}
---
+++ src/test/java/SwitchTest/SwitchTests1.java
@@ -0,0 +1,5 @@
+package SwitchTest;
+
+public class SwitchTests1 {
+
+}
